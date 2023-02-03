4c0c27d0a6d2f4d0b912ec620da00787cb85912a
aldwiputra <aldiansyhdp@gmail.com>
2023-02-03 10:36:28 +0700

    add mobile menu reveal animation

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuMobile = document.querySelector('.menu-mobile');
 
 hamburgerMenu.addEventListener('click', menuClickHandler);
 
 function menuClickHandler(event) {
-	hamburgerMenu.classList.toggle('active');
-	hamburgerMenu.blur();
+    hamburgerMenu.classList.toggle('active');
+    menuMobile.classList.toggle('active');
+    hamburgerMenu.blur();
 }
