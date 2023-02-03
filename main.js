edaea1fc0e1a9f90e999608b41f85e5484f32b99
aldwiputra <aldiansyhdp@gmail.com>
2023-02-03 13:14:56 +0700

    add out transition for menu

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const menuMobile = document.querySelector('.menu-mobile');
 hamburgerMenu.addEventListener('click', menuClickHandler);
 
 function menuClickHandler(event) {
+    if (menuMobile.classList.contains('active')) {
+        menuMobile.classList.add('out');
+    }
+
     hamburgerMenu.classList.toggle('active');
     menuMobile.classList.toggle('active');
     hamburgerMenu.blur();
+
+    menuMobile.classList.remove('out');
 }
